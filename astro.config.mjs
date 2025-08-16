286fa11a0fdc2dd40844084a63b36a4e96a8b19b
Junkwang Kim <kjk1208@dgist.ac.kr>
2025-08-16 20:59:23 +0900

    astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,11 @@ import expressiveCode from 'astro-expressive-code';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 
-// Learn more: https://docs.astro.build
 export default defineConfig({
   site: 'https://kjk1208.github.io',
   integrations: [
-    mdx(),
-    expressiveCode()
+    expressiveCode(),
+    mdx()
   ],
   markdown: {
     remarkPlugins: [remarkMath],
